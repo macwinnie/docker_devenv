8e66b54f5e86280590cf0410aef28ba847940ef4
Martin Winter <git@macwinnie.me>
2018-07-06 02:09:08 +0200

    reorganise

--- container/default/phpmyadmin.sh
+++ container/default/phpmyadmin.sh
@@ -8,11 +8,12 @@ fi
 source $SCRIPT_PATH/config/require.sh
 
 isContainerRunning "system/traefik"
+isContainerRunning "system/database"
 
-docker_name="system.portainer"
-image="portainer/portainer"
+docker_name="default.phpmyadmin"
+image="phpmyadmin/phpmyadmin"
 
-local_domain='portainer.local'
+local_domain='pma.local'
 register_host $local_domain
 
 if checkRunning "$docker_name"; then
@@ -20,14 +21,18 @@ if checkRunning "$docker_name"; then
     docker run --detach \
       --name $docker_name \
       --restart always \
-      --volume /var/run/docker.sock:/var/run/docker.sock:rw \
-      --volume $DATA_PATH/system/portainer/data:/data:rw \
+      --volume $DATA_PATH/system/phpmyadmin/config.user.inc.php:/etc/phpmyadmin/config.user.inc.php:ro \
+      --env PMA_HOST=database \
+      --env PMA_USER=root \
+      --env PMA_PASSWORD=Def12345 \
+      --label conf \
       --label traefik.frontend.rule="Host:$local_domain" \
       --label traefik.frontend.entryPoints=http \
       --label traefik.docker.network=$NETWORK_TRAEFIK \
-      --label traefik.backend="system: Portainer" \
-      --label traefik.port=9000 \
+      --label traefik.backend="default: phpMyAdmin" \
+      --label traefik.port=80 \
       $image
 
+    controllNetwork "internal" "$docker_name"
     controllNetwork "traefik" "$docker_name"
 fi
--- container/default/portainer.sh
+++ container/default/portainer.sh
@@ -8,12 +8,11 @@ fi
 source $SCRIPT_PATH/config/require.sh
 
 isContainerRunning "system/traefik"
-isContainerRunning "system/database"
 
-docker_name="system.phpmyadmin"
-image="phpmyadmin/phpmyadmin"
+docker_name="default.portainer"
+image="portainer/portainer"
 
-local_domain='pma.local'
+local_domain='portainer.local'
 register_host $local_domain
 
 if checkRunning "$docker_name"; then
@@ -21,18 +20,14 @@ if checkRunning "$docker_name"; then
     docker run --detach \
       --name $docker_name \
       --restart always \
-      --volume $DATA_PATH/system/phpmyadmin/config.user.inc.php:/etc/phpmyadmin/config.user.inc.php:ro \
-      --env PMA_HOST=database \
-      --env PMA_USER=root \
-      --env PMA_PASSWORD=Def12345 \
-      --label conf \
+      --volume /var/run/docker.sock:/var/run/docker.sock:rw \
+      --volume $DATA_PATH/system/portainer/data:/data:rw \
       --label traefik.frontend.rule="Host:$local_domain" \
       --label traefik.frontend.entryPoints=http \
       --label traefik.docker.network=$NETWORK_TRAEFIK \
-      --label traefik.backend="system: phpMyAdmin" \
-      --label traefik.port=80 \
+      --label traefik.backend="default: Portainer" \
+      --label traefik.port=9000 \
       $image
 
-    controllNetwork "internal" "$docker_name"
     controllNetwork "traefik" "$docker_name"
 fi
