4731a369834c1164cb0dc426eae22bb819ae011e
Martin Winter <mwinter@it-economics.de>
2018-09-07 15:15:01 +0200

    added passcard configuration file to repository

---
+++ init/container/it-e/passcard.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z ${SCRIPT_PATH+x} ]; then
+    echo "do not run this script directly!"
+    exit 1
+fi
+
+source $SCRIPT_PATH/config/require.sh
+isContainerRunning "system/traefik"
+#isContainerRunning "system/database"
+
+docker_name="it-e.passcard"
+image="iteconomics/apache:php7.2"
+
+local_domain='passcard.local'
+register_host $local_domain
+
+if checkRunning "$docker_name"; then
+    docker pull $image
+    docker run --detach \
+      --name $docker_name \
+      --restart unless-stopped \
+      --volume $DATA_PATH/it-e/passcard/laravel:/var/www/html:rw \
+      --volume $DATA_PATH/it-e/passcard/logs:/var/log/apache2:rw \
+      --env PHP_XDEBUG=1 \
+      --env XDEBUG_IDE_KEY="$local_domain" \
+      --env APACHE_PUBLIC_DIR="/var/www/html/public" \
+      --label traefik.frontend.rule="Host:$local_domain" \
+      --label traefik.frontend.entryPoints=http \
+      --label traefik.docker.network=$NETWORK_TRAEFIK \
+      --label traefik.backend="it-e: passcard" \
+      --label traefik.port=80 \
+      $image
+
+    echo -e "\033[31mDo not forget to check out PassCard sourcecode:"
+    echo -e "git clone ssh://git@bitbucket.it-economics.de:7999/infra/passcard.git $DATA_PATH/it-e/passcard/laravel\033[0m"
+
+    controllNetwork "internal" "$docker_name"
+    controllNetwork "traefik" "$docker_name"
+fi
